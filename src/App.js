e438f773f512a86cde483d9b79ba4c014a286b6c
guerrero-jorge <guerrerov.jorge@gmail.com>
2022-02-28 16:44:41 -0500

    changed Language

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
   const colors=["#808080","#000080","#0000FF","#008080","#FF0000","#008000"]
   /*const colors=["#845EC2","#C197FF","#00C9A7","#005B44","#FF6F91","#FF8066"]*/
   
-  const[cita,setCita]=useState(quotes[getNumber(quotes.length)]);
+  const[quote,setQuote]=useState(quotes[getNumber(quotes.length)]);
   const[color,setColor]=useState(colors[getNumber(colors.length)])
 
   const handleCita= () => {
 
-    setCita(quotes[getNumber(quotes.length)]);
+    setQuote(quotes[getNumber(quotes.length)]);
     setColor(colors[getNumber(colors.length)]);
 
   }
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="App" style={{background:color}}>
 
-      <Card  quote={cita.quote} author={cita.author} handleCita={handleCita} color={color}/>
+      <Card  quote={quote.quote} author={quote.author} handleCita={handleCita} color={color}/>
 
     </div>
   );
